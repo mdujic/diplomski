91c6882d8210395f67ab227533f8bc781191ac56
mateo <mdujic212@gmail.com>
2023-06-26 01:07:12 +0200

    feat: finished clean implementation of first two algorithms, all_restricted to do

--- all_shortest_paths/tests/test_main.py
+++ all_shortest_paths/tests/test_main.py
@@ -1,38 +1,56 @@
 from all_shortest_paths.graph_database.graph import Graph
 
+graph = Graph()
+graph.add_edge(0, 1, 'a')
+graph.add_edge(0, 2, 'b')
+graph.add_edge(0, 5, 'a')
+graph.add_edge(1, 3, 'a')
+graph.add_edge(1, 4, 'b')
+graph.add_edge(2, 1, 'a')
+graph.add_edge(2, 4, 'b')
+graph.add_edge(3, 2, 'a')
+graph.add_edge(3, 4, 'b')
 
-def test_rpq_eval():
-    graph = Graph()
-    graph.add_edge(0, 1, 'a')
-    graph.add_edge(0, 2, 'b')
-    graph.add_edge(0, 5, 'a')
-    graph.add_edge(1, 3, 'a')
-    graph.add_edge(1, 4, 'b')
-    graph.add_edge(2, 1, 'a')
-    graph.add_edge(2, 4, 'b')
-    graph.add_edge(3, 2, 'a')
-    graph.add_edge(3, 4, 'b')
-
-    graph.end_inserts()
-
-    assert graph.rpq_eval(0, 'ab') == [[0, 1, 4]]
+graph.end_inserts()
 
 
-def test_all_shortest_rpq_eval():
-    graph = Graph()
-    graph.add_edge(0, 1, 'a')
-    graph.add_edge(0, 2, 'b')
-    graph.add_edge(0, 5, 'a')
-    graph.add_edge(1, 3, 'a')
-    graph.add_edge(1, 4, 'b')
-    graph.add_edge(2, 1, 'a')
-    graph.add_edge(2, 4, 'b')
-    graph.add_edge(3, 2, 'a')
-    graph.add_edge(3, 4, 'b')
+def test_any_walk():
+    assert graph.any_walk(0, 'a*') == [
+        [0],
+        [0, (0, 'a', 1), 1],
+        [0, (0, 'a', 5), 5],
+        [0, (0, 'a', 1), 1, (1, 'a', 3), 3],
+        [0, (0, 'a', 1), 1, (1, 'a', 3), 3, (3, 'a', 2), 2]
+    ]
 
-    graph.end_inserts()
+def test_any_shortest_walk():
+    assert graph.any_walk(0, '(a+b)*', True) == [
+        [0],
+        [0, (0, 'a', 1), 1],
+        [0, (0, 'a', 5), 5],
+        [0, (0, 'b', 2), 2],
+        [0, (0, 'a', 1), 1, (1, 'a', 3), 3],
+        [0, (0, 'a', 1), 1, (1, 'b', 4), 4]
+    ]
 
-    assert graph.all_shortest_rpq_eval(0, 'aa+ab+ba+bb') == [[0, 2, 4], [0, 2, 1], [0, 1, 4], [0, 1, 3]]
+def test_all_shortest_walk():
+    assert graph.all_shortest_walk(0, 'a*') == [
+        [
+            [0]
+        ],
+        [
+            [0, (0, 'a', 1), 1]
+        ],
+        [
+            [0, (0, 'a', 5), 5]
+        ],
+        [
+            [0, (0, 'a', 1), 1, (1, 'a', 3), 3]
+        ],
+        [
+            [0, (0, 'a', 1), 1, (1, 'a', 3), 3, (3, 'a', 2), 2]
+        ]
+    ]
 
 
 def test_all_shortest_huge():
@@ -44,6 +62,6 @@ def test_all_shortest_huge():
         graph.add_edge(i, i+1, 'b')
 
     graph.end_inserts()
-    
+
     count = len(graph.all_shortest_rpq_eval(0, '(a+b)*'))
     assert count == a + 1
